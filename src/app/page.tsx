1d89c859fc6b8c60ca5932a341270a8db5cd01da
caiokronuz <caiogabriel135@gmail.com>
2024-06-25 10:25:19 -0300

    adicionado redirecionamento de usuários logados para a dashboard e adicionado botão na landind page para um cliente poder abrir um chamado sem login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,19 @@
 import Image from "next/image";
+import {redirect} from 'next/navigation'
+import Link from "next/link";
+
+import { auth } from "@/lib/auth";
 
 import heroImg from '@/assets/hero.svg';
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await auth();
+
+  if(session?.user){
+    redirect("/dashboard")
+  }
+
   return (
     <main className="flex flex-col justify-center items-center min-h-[calc(100vh-80px)]">
       <h2 className="font-medium text-2xl mb-2">Gerencie sua empresa</h2>
@@ -13,6 +24,7 @@ export default function Home() {
         width={600}
         className="max-w-sm md:max-w-xl"
       />
+      <Link href="/open" className="bg-blue-600 p-2 rounded text-white mt-8 hover:scale-105 transition">Sou cliente e quero abrir um chamado</Link>
     </main>
   );
 }
